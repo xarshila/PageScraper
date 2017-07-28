3c922794eff45077065f2e136a2479925162fa4b
Lasha Kharshiladze <hunger-sona@mail.ru>
2017-07-29 02:03:16 +0400

    Added Main menu

--- src/WebScraperApp.java
+++ src/WebScraperApp.java
@@ -1,18 +1,62 @@
 import java.util.*;
 public class WebScraperApp {
-    private static final String SENTINEL = "quit";
+    private static final String SENTINEL    = "QUIT";
+    private static final String IMG_OPTION  = "IMG";
+    private static final String LINK_OPTION = "LINK";
     
-    
+    SoupScraper scraper;
+    Scanner sc ;
+    public WebScraperApp(){
+        printHeader();
+        scraper = new SoupScraper();
+        sc = new Scanner(System.in);
+        runMainMenu();
+        sc.close();
+    }
+    /**
+     * Console Application for Web Scrapper
+     * @param args
+     */
     public static void main(String[] args){
+        WebScraperApp  app = new WebScraperApp();
+    }
+    
+    /**
+     * Print Welocme Text and Instruction
+     */
+    private void printHeader(){
         System.out.println("Welcome WebScraper Console Applcation!");
-        System.out.println("-------------------------------------");
-        System.out.println("Write arbitrary url and Scraper lists down \n"
-                 + "all Page Links on that URL (wirte " + SENTINEL +" for exit)");
-        
-        SoupScraper scraper = new SoupScraper();
-        Scanner sc = new Scanner(System.in);
-        
+    }
+    
+    /**
+     * Take Options you what scrap images or URLS
+     */
+    private  void runMainMenu(){
         while(true){
+            System.out.println("-------------  MAIN MENU  --------------");
+            System.out.println("Write '" + LINK_OPTION + "' for link Scrapin");
+            System.out.println("Write '" + IMG_OPTION + "' for Image Scrapin");
+            System.out.println("Write '" + SENTINEL + "' for esc");
+            System.out.println("-----------------------------------------");
+            
+            String line = sc.nextLine();
+            if(line.equals(SENTINEL))
+                break;
+            if(line.equals(LINK_OPTION))
+                runLinkScraping();
+            if(line.equals(IMG_OPTION))
+                runImageScraping();
+        }
+    }
+    
+    /**
+     * Reads URL from console and lists Image scrcs
+     * for that url
+     */
+    private void runImageScraping(){
+        System.out.println("---------- Image Scraping -----------");
+        while(true){
+            
             System.out.print("URL:");
             String url = sc.nextLine();
             if(url.equals(SENTINEL))
@@ -23,18 +67,40 @@ public class WebScraperApp {
             }
             printUrlList(pageLinks);
         }
-        sc.close();
     }
+    
+    /**
+     * Reads URL from console and lists Image scrcs
+     * for that url
+     */
+    private void runLinkScraping(){
+        System.out.println("---------- Link Scraping -----------");
+        while(true){
+            System.out.print("URL:");
+            String url = sc.nextLine();
+            if(url.equals(SENTINEL))
+                break;
+            List<String> pageLinks = scraper.getPageLinks(url);
+            if(pageLinks == null){
+                continue;
+            }
+            printUrlList(pageLinks);
+        }
+    }
+    
     
     /**
      * prints given List with indexes
      * @param urls
      */
     private static void printUrlList(List<String> urls){
+        System.out.println("----------" + urls.size() + " Result Founded --------");
         int ind = 1;
         for(String url: urls){
             System.out.println(ind + ") "+ url);
             ind++;
         }
+        System.out.println("------------------------------------------------------");
+        System.out.println();
     }
 }
