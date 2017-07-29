db9b7d9f34d2803c2b90a0f0e6990a60ed8ecc8f
Lasha Kharshiladze <hunger-sona@mail.ru>
2017-07-29 16:08:24 +0400

    Added Scrap Worker Class which scraps url in new thread puts result in result table model

---
+++ src/ScrapWorker.java
@@ -0,0 +1,103 @@
+import java.util.List;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * ScrapWorker class is responsible for:
+ *  -> scraping given url in new Thread
+ *  -> adding results in resultTableModel from Swing Thread
+ * 
+ * @author Lasha Kharshiladze
+ */
+class ScrapWorker implements Runnable{
+        private static final int SCRAP_DELAY = 80;
+        
+        // references from outside
+        String           url;
+        SoupScraper      scraper;
+        ResultTableModel resultTableModel;
+        boolean images;
+        boolean links;
+        
+        
+        /**
+         * Defualt Constructor
+         * 
+         * @param resultTableModel
+         * @param scraper
+         * @param url
+         * @param images
+         * @param links
+         */
+        public ScrapWorker(ResultTableModel resultTableModel, SoupScraper scraper,String url, boolean images, boolean links){
+            this.url     = url;
+            this.scraper = scraper;
+            this.images  = images;
+            this.links   = links;
+            this.resultTableModel = resultTableModel;
+        }
+        
+        @Override
+        public void run(){
+            if(images)
+                scrapImages(url);
+            if(links)
+                scrapLinks(url);
+        }
+        
+        /**
+         * Scraps image for url in new Thread
+         * @param url to scrap
+         */
+        private void scrapImages(String url){
+            List<String> imageSrcs = scraper.getImageSrcs(url);
+            if(imageSrcs == null)
+                return; 
+            for(String src : imageSrcs){
+                try {
+                    Thread.sleep(SCRAP_DELAY);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                SwingUtilities.invokeLater(new Runnable(){
+                    @Override
+                    public void run() {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                               resultTableModel.add(src, "img");
+                            }
+                        });
+                    }
+                    
+                });
+            }
+        }
+        
+        /**
+         * Scraps link for url in new Thread
+         * @param url to scrap for links
+         */
+        public void scrapLinks(String url){
+            List<String> pageLinks = scraper.getPageLinks(url);
+            if(pageLinks == null)
+                return;
+            for(String link : pageLinks){
+                try {
+                    Thread.sleep(SCRAP_DELAY);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                SwingUtilities.invokeLater(new Runnable(){
+                    @Override
+                    public void run() {
+                        SwingUtilities.invokeLater(new Runnable() {
+                            public void run() {
+                               resultTableModel.add(link, "link");
+                            }
+                        });
+                    }
+                    
+                });
+            }
+        }
+    }
